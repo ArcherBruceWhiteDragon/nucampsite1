481726cdc7dd152f00528b9377e7776f89c79fb7
Siv Taing <sivlongtaing@gmail.com>
2021-02-02 00:28:31 -0800

    About/Contact Components commit

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
-import { Image, ScrollView } from 'react-native';
-
+import Constants from 'expo-constants';
+import { useState } from 'react';
 
 class Contact extends Component {
   constructor(props) {
@@ -17,8 +17,43 @@ class Contact extends Component {
   render() {
   return(
     <ScrollView>
+      <Card title='Contact Information' wrapperStyle={{margin: 10}}>    
+      </Card>
     </ScrollView>
   )
+  } 
+}
+
+class TextInANest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      titleText: "Bird's Nest",
+      bodyText: "This is not really a bird nest."
+    };
+  }
+  render() {
+    return (
+      <Text style={styles.baseText}>
+        <Text style={styles.titleText} onPress={this.onPressTitle}>
+          {this.state.titleText}
+          {"\n"}
+          {"\n"}
+        </Text>
+        <Text numberOfLines={5}>{this.state.bodyText}</Text>
+      </Text>
+    );
   }
 }
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: "Cochin"
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+
+
 export default Contact;
